13428fa0c74376f3aab66c942c054bc93780d3ef
Santanu Biswas <shanpro.2015@gmail.com>
2016-06-28 12:16:57 +0530

    Add files via upload

--- sh-easy-social-like-button-widget.php
+++ sh-easy-social-like-button-widget.php
@@ -54,6 +54,7 @@
         }
         
         function load_scripts() {
+            wp_enqueue_style( 'style_sheet', plugin_dir_url(__FILE__).'shwsbw_style_test.css', '' );
             wp_enqueue_style( 'style_sheet', plugin_dir_url(__FILE__).'shwsbw_style.css', '' );
             wp_enqueue_script( 'upload_jquery', plugin_dir_url(__FILE__).'shwsbw_script.js', array( 'jquery' ), sh_easy_social_button_widget_version, true );
         }
